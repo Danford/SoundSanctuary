4d223b6ed0d7285c8f6be687bc706262ef56879b
Matt <technoman1227@gmail.com>
2020-02-01 12:51:41 -0400

    Breaking apart the javascript and CSS so it'll be easier to have multiple people working on it.

--- htdocs/js/game.js
+++ htdocs/js/game.js
@@ -1,45 +1,36 @@
 /**
 	Responsible for pulling everything together and the core game logic.
-	Version: 11:50PM AST 1/31/2020
 */
 
-/// Configure our phaser instance
-var config = {
-	type : Phaser.AUTO,
-	parent : 'game-container',
-	width : window.innerWidth,
-	height : window.innerHeight,
-	scene : {
-		preload : preload,
-		create : create
-	},
-	pixelArt : true,
-	audio : {
-		disableWebAudio : true
-	}
+ /**
+	Init the scene and assets for render.
+	@param HTMLElement canvas The <canvas> element that we should render to.
+	@param Babylon.Engine engine The BabylonJS engine instance to add the scene too.
+	
+	@return Babylon.Scene The newly created and populated scene.
+ */
+function createScene(canvas, engine) {
+	// Create the scene space
+	var scene = new BABYLON.Scene(engine);
+	canvas.style.width = '100%';
+	canvas.style.height = '100%';
+
+	// Add a camera to the scene and attach it to the canvas
+	var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -10), scene);
+
+	// Targets the camera to a particular position. In this case the scene origin
+	camera.setTarget(BABYLON.Vector3.Zero());
+
+	// Attach the camera to the canvas
+
+	camera.attachControl(canvas, true);
+
+	// Add lights to the scene
+	var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+	var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+
+	// Add and manipulate meshes in the scene
+	var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
+
+	return scene;
 };
-
-var game = new Phaser.Game(config);
-
-/**
-	Responsible for preloading assets.
-*/
-function preload(){
-	this.load.setBaseURL('assets');
-	
-	this.load.image('title', 'images/title.png');
-	
-	//this.load.spritesheet(); // TODO, get assets
-	
-	//this.load.audioSprite(); // TODO, get sound effects
-	
-	this.load.audio('radio_song', 'music/Lobo_Loco_-_02_-_Traveling_to_Lousiana_-_Soft_Delay_ID_1174.mp3');
-}
-
-/**
-	Responsible for creating the first scene.
-*/
-function create(){
-	
-}
-
